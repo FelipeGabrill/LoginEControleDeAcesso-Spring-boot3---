498b1c26e604cd15696e0b5f0e71f41544b5d506
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-05-17 07:41:54 -0300

    BCrypt password encoder

--- spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$/JwGeiyqVk5JFi9UC2HSXOJ7/MXixM0PMeHRG3Ifgmnkg0AEuL4Za');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$/JwGeiyqVk5JFi9UC2HSXOJ7/MXixM0PMeHRG3Ifgmnkg0AEuL4Za');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERADOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
