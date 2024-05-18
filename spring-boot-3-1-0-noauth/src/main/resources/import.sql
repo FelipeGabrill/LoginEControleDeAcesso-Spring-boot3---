eb303308693cb4ff1400e1492f9fa2b69b283dea
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-05-18 16:25:41 -0300

    Controle de acesso por perfil e rota

--- spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,7 +1,7 @@
 INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$/JwGeiyqVk5JFi9UC2HSXOJ7/MXixM0PMeHRG3Ifgmnkg0AEuL4Za');
 INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$/JwGeiyqVk5JFi9UC2HSXOJ7/MXixM0PMeHRG3Ifgmnkg0AEuL4Za');
 
-INSERT INTO tb_role (authority) VALUES ('ROLE_OPERADOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
